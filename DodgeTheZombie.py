a652b13453a0031dc883be108687ef552ff27e67
harnoorjosan <91575135+harnoorjosan@users.noreply.github.com>
2024-04-12 12:54:00 -0600

    Skeleton of classes
    Skeleton of classes (Zombie and Frontier)

--- DodgeTheZombie.py
+++ DodgeTheZombie.py
@@ -1,1 +1,24 @@
 "Dodge the Zombie Problem"
+
+class Zombie_problem(object):
+    pass
+
+class Frontier(object):
+    """
+    Convenience wrapper for a priority queue usable as a frontier
+    implementation.
+    """
+    pass
+
+def unit_tests():
+    """
+    Some trivial tests to check that the implementation even runs.
+    Feel free to add additional tests.
+    """
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
