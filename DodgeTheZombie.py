a50d8f87dedc21f1e651b463f1120c2497ed2ad3
harnoorjosan <91575135+harnoorjosan@users.noreply.github.com>
2024-04-13 22:19:01 -0600

    Problem Representation and Frontier
    Zombie problem representation done and frontier implementation finished.

--- DodgeTheZombie.py
+++ DodgeTheZombie.py
@@ -1,14 +1,43 @@
 "Dodge the Zombie Problem"
+from heapq import heappush, heappop
 
 class Zombie_problem(object):
-    pass
+    def __init__(self):
+        self.people_cost = {'undergrad': 1, 'grad': 2, 'postdoc': 5, 'professor': 10}
+        self.people = ['undergrad', 'grad', 'postdoc', 'professor']
+        
+    def start_node(self):
+        """returns start node"""
+        start = ('left', 'left', 'left', 'left', 'left') # Everyone on the left side of the bridge
+        return (start)
+    
+    
+    def is_goal(self, node):
+        """is True if `node` is a goal"""
+        return node == ('right', 'right', 'right', 'right', 'right') # Everyone on the right side of the bridge
 
 class Frontier(object):
     """
     Convenience wrapper for a priority queue usable as a frontier
     implementation.
     """
-    pass
+    def __init__(self):
+        self.heap = []
+
+    def add(self, path, priority):
+        """Add `path` to the frontier with `priority`"""
+        # Push a ``(priority, item)`` tuple onto the heap so that `heappush`
+        # and `heappop` will order them properly
+        heappush(self.heap, (priority, path))
+
+    def remove(self):
+        """Remove and return the smallest-priority path from the frontier"""
+        (priority, path) = heappop(self.heap)
+        return path
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
 
 def unit_tests():
     """
